187ef7071f53226bb36fb6fed03374421f7f5a43
Malek <pocmalek@gmail.com>
2025-04-25 17:36:46 -0400

    uwu

---
+++ examples/test.rs
@@ -0,0 +1,32 @@
+use bevy::asset::AssetMetaCheck;
+use bevy::prelude::*;
+use bevy_u8_assets::{U8AssetPlugin, U8AssetWriter};
+
+pub fn main() {
+    App::new()
+        .add_plugins(U8AssetPlugin)
+        .add_plugins(DefaultPlugins.set(AssetPlugin {
+            meta_check: AssetMetaCheck::Never,
+            ..AssetPlugin::default()
+        }))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut writer: ResMut<U8AssetWriter>,
+    asset_server: Res<AssetServer>,
+) {
+
+    let bytes =
+        std::fs::read("assets/Grass_01.png")
+            .unwrap();
+    writer.write("grass.png", &[]);
+    commands.spawn(Camera2d);
+    commands.spawn(Sprite {
+        image: asset_server.load("u8://grass.png"),
+        ..default()
+    });
+    writer.write_all("grass.png", bytes);
+}
